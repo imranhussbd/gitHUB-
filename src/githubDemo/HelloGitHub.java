0c39115f4417460dd48b3282b7972fdefed17593
imranhussbd <imranhussbd@gmail.com>
2022-05-28 00:46:09 -0400

    works

--- src/githubDemo/HelloGitHub.java
+++ src/githubDemo/HelloGitHub.java
@@ -4,6 +4,6 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println(
 				"I know how to create a separate remote repository in Github and can push my project GithubDemo, without help of anyone");
-	}
+	}//works
 
 }
